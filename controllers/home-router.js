318482eff52cabafc7662ba0f54f017e92964cf5
LemonPocky <someone-70@live.com>
2021-07-29 12:54:54 -0700

    Created /profile/id route

--- controllers/home-router.js
+++ controllers/home-router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../models");
+const { User, Spot, Tag, SpotTag } = require("../models");
 
 // use withAuth middleware to redirect from protected routes.
 // const withAuth = require("../util/withAuth");
@@ -29,10 +29,48 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/profile/:id", (req, res) => {
-  // When rendering, check if the :id matches the user id of the current session
-  // If so, display a logout button
-  res.render("profile", { title: "User's Profile" });
+// Route to display a user's profile
+router.get("/profile/:id", async (req, res) => {
+  try {
+    // Find the user with this id
+    // Return a 404 if there is no user with this id
+    const userData = await User.findByPk(req.params.id, {
+      attributes: ["id", "username", "bio"],
+    });
+
+    const user = userData.get({ plain: true });
+
+    if (!user) {
+      res.sendStatus(404).json("No user with that id found!");
+    }
+
+    // Find all spots that contain the user_id :id
+    // But don't return any user data with this query, we get it in the last one
+    const userSpotsData = await Spot.findAll({
+      include: {
+        model: User,
+        where: {
+          id: req.params.id,
+        },
+        attributes: [],
+        required: true,
+      },
+    });
+    const userSpots = userSpotsData.get({ plain: true });
+
+    // Check if the :id matches the user id of the current session
+    // If so, display a logout button
+    const matchesSessionUser = req.session.user_id === req.params.id;
+
+    res.render("profile", {
+      title: `${user.username}'s Profile`,
+      user,
+      spots: userSpots,
+      matchesSessionUser,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.get("/login", (req, res) => {
