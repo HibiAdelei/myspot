2250f6da27ae5791ef397bdd0bfbc87650cbe7b1
LemonPocky <someone-70@live.com>
2021-07-29 14:54:41 -0700

    Added some feedback messages to let the user know if the bio was saved successfully

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,23 +2,29 @@
 const handleBioSubmit = async (event) => {
   event.preventDefault();
   try {
-    const bio = document.querySelector("#user-bio-text").value;
+    const bio = document.querySelector(".bio");
+    const bioText = document.querySelector("#user-bio-text").value;
 
     const response = await fetch("/api/users", {
       method: "UPDATE",
-      body: JSON.stringify({ bio }),
+      body: JSON.stringify({ bioText }),
       headers: {
         "Content-Type": "application/json; charset=UTF-8",
       },
     });
 
+    // Display a message on screen if the save was successful
+    // or not
     if (!response.ok) {
-      alert("Error saving user bio.");
-      return;
+      bio.classList.remove("bio-form-success");
+      bio.classList.add("bio-form-error");
+    } else {
+      bio.classList.remove("bio-form-error");
+      bio.classList.add("bio-form-success");
     }
   } catch (error) {
     console.log(error);
   }
 };
 
-document.querySelector(".bio-form").addEventListener("submit", handleBioSubmit);
+document.querySelector(".bio").addEventListener("submit", handleBioSubmit);
