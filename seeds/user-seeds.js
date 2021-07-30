11344f10df7c4ed37334899a1a604edd3ee5dd16
wooglow <74934952+wooglow@users.noreply.github.com>
2021-07-30 12:59:48 -0700

    Update user-seeds.js

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,16 +2,19 @@ const { User } = require('../models');
 
 const userData = [
     {
+        id: 1,
         username: "Lily",
         bio: "Team MySpot",
         password: "asdf1234"
     },
     {
+        id: 2,
         username: "Lilian",
         bio: "Team MySpot",
         password: "qwer1234"
     },
     {
+        id: 3,
         username: "Daeyoung",
         bio: "Team MySpot",
         password: "zxcv1234"
