4c86b917eb64ec7fd677ec1a5407c536307fc90f
LemonPocky <someone-70@live.com>
2021-08-01 13:10:17 -0700

    Renamed new.js to new-spot.js, created a fetch request to post the new spot

---
+++ public/js/new-spot.js
@@ -0,0 +1,30 @@
+const newSpotHandler = async function (event) {
+  event.preventDefault();
+
+  const title = document.getElementById('title').value;
+  const longitude = document.getElementById('longitude').value;
+  const latitude = document.getElementById('latitude').value;
+  const description = document.getElementById('description').value;
+
+  // Get tags into an array
+  const selectedTags = document.getElementById('tags').selectedOptions;
+  const tags = Array.from(selectedTags).map(({ value }) => value);
+
+  const newSpot = await fetch('/api/spot', {
+    method: 'POST',
+    body: JSON.stringify({
+      title,
+      latitude,
+      longitude,
+      description,
+      tags,
+    }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  // Redirect to newly created spot
+  const newSpotJson = await newSpot.json();
+  window.location.href = `/spot/${newSpotJson.id}`;
+};
+
+document.querySelector('#spot-form').addEventListener('submit', newSpotHandler);
